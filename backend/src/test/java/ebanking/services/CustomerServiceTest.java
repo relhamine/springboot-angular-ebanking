7260f379eb2f9a7910e9973b55761df4f865ab24
Rachid El Hamine <relhamine@gmail.com>
2025-04-23 17:29:19 +0200

    add new project ebanking (Sprng boot 3, angular 18, docker

--- backend/src/test/java/ebanking/services/CustomerServiceTest.java
+++ backend/src/test/java/ebanking/services/CustomerServiceTest.java
@@ -3,34 +3,50 @@ package ebanking.services;
 import ebanking.entities.Customer;
 import ebanking.repositories.CustomerRepository;
 import ebanking.services.impl.CustomerServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class CustomerServiceTest {
 
-    @Autowired
+    @Mock
     private CustomerRepository customerRepository;
 
-    @Autowired
+    @InjectMocks
     private CustomerServiceImpl customerService;
 
 
-    @BeforeEach
     void setUp() {
     }
 
-
     @Test
-    void saveCustomer() {
+    void shouldReturnListCustomers() {
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setName("dupont");
+        customer1.setEmail("dupont@gmaill.com");
+
+        Customer customer2 = new Customer();
+        customer2.setId(1l);
+        customer2.setName("dupont2");
+        customer2.setEmail("dupont2@gmaill.com");
+
+        when(customerRepository.findAll()).thenReturn(List.of(customer1, customer2));
+
+        List<Customer> customers = customerRepository.findAll();
+        assertThat(customers).hasSize(2);
     }
 
     @Test
-    void listCustomers() {
+    void saveCustomer() {
     }
 
     @Test
